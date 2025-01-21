89111ccda358c2f284fec6e6e4696d777da4f7d9
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-01-22 00:29:58 +0600

    Add logo image to the coming soon page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import Logo from "@/public/logo.svg";
+import Image from "next/image";
 export const metadata = {
   title: "Coming Soon | Andcodex IT Solutions",
 };
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <div className="text-center">
+        <Image
+          src={Logo}
+          alt="Andcodex IT Solutions"
+          width={100}
+          height={100}
+          className="mx-auto mb-4 lg:mb-6"
+        />
+
         <h1 className="text-4xl md:text-6xl font-bold text-red-500 uppercase mb-4">
           Coming Soon
         </h1>
